caec3ae2ec18d82842f136332b4e0ee78d1536a8
billdevmaster71 <billdevmaster@gmail.com>
2023-06-05 17:23:51 +0900

    fix: update for node module

--- laravel/app/Http/Controllers/AppController.php
+++ laravel/app/Http/Controllers/AppController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
+  private $env_text = "";
   /**
    * Create a new controller instance.
    *
@@ -21,6 +22,7 @@ class AppController extends Controller
     chdir('../../../');
     exec("git clone https://github.com/billdevmaster/my-history");
     rename('my-history', $request->input('app_data')['APP_NAME']);
+    copy('PATTERN/index.php', $Username.'/index.php');
     var_dump($request->input('app_data'));
   }
 }
