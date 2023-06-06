ce206b51a466d9ea257dc0b975d56aa42a5131b8
billdevmaster71 <billdevmaster@gmail.com>
2023-06-06 17:22:53 +0900

    fix: git clone git repo

--- laravel/app/Http/Controllers/AppController.php
+++ laravel/app/Http/Controllers/AppController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class AppController extends Controller
 {
   private $env_text = "";
+  private $git_repo = "https://github.com/billdevmaster/my-history";
   /**
    * Create a new controller instance.
    *
@@ -19,10 +20,10 @@ class AppController extends Controller
 
   public function create(Request $request)
   {
-    chdir('../../../');
-    exec("git clone https://github.com/billdevmaster/my-history");
+    chdir(env('NEW_APP_DIR'));
+    exec("git clone " . $this->git_repo);
     rename('my-history', $request->input('app_data')['APP_NAME']);
-    copy('PATTERN/index.php', $Username.'/index.php');
+    // copy('PATTERN/index.php', $Username.'/index.php');
     var_dump($request->input('app_data'));
   }
 }
