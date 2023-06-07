24ba30bf99e5de13960cd028cb4aab7c78d03465
billdevmaster71 <billdevmaster@gmail.com>
2023-06-07 09:34:38 +0900

    fix: update for booking

--- laravel/app/Http/Controllers/AppController.php
+++ laravel/app/Http/Controllers/AppController.php
@@ -8,6 +8,7 @@ class AppController extends Controller
 {
   private $env_text = "";
   private $git_repo = "https://github.com/billdevmaster/booking_demo";
+  
   /**
    * Create a new controller instance.
    *
@@ -22,8 +23,7 @@ class AppController extends Controller
   {
     chdir(env('NEW_APP_DIR'));
     exec("git clone " . $this->git_repo);
-    exec("mv my-history " . $request->input('app_data')['APP_NAME']);
-    // rename('my-history', $request->input('app_data')['APP_NAME']);
+    rename('booking_demo', $request->input('app_data')['APP_NAME']);
     // copy('PATTERN/index.php', $Username.'/index.php');
     var_dump($request->input('app_data'));
   }
